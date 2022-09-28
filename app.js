906d77e23e97fb4551f1d2a2552c42a00c49cdf6
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-28 11:05:19 +0530

    added express route methods.

--- app.js
+++ app.js
@@ -6,15 +6,30 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.set('view engine',ejs);
 app.use(express.static('public'));
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(PORT,(err)=>{
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.get('/login',function(req,res){
+    res.render('login');
+});
+app.get('/register',function(req,res){
+    res.render('register');
+});
+
+
+
+
+app.listen(PORT, function(err){
     if(!err){
         console.log('server started on port '+PORT+' bro!');
     }
     else{
-        console.log(err);
+        console.log('There was some error while connecting to the server | port bro'+err);
     }
+        
 });

6ccd25ccbe160972c1e9af7f74c7a0145c8bb054
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-28 10:56:55 +0530

    added express file to handle requests made to server(express).

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine',ejs);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(PORT,(err)=>{
+    if(!err){
+        console.log('server started on port '+PORT+' bro!');
+    }
+    else{
+        console.log(err);
+    }
+});

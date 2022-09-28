c9119ed4e6385d6bc469be55ae2751b47aa49c6e
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-28 13:29:44 +0530

    added md5 hashing to our passwords both at registration and login.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -28,7 +29,7 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt, {secret:process.env.MYSECRET, encryptedFields:['password']});
+// userSchema.plugin(encrypt, {secret:process.env.MYSECRET, encryptedFields:['password']});
 
 const User = new mongoose.model('User',userSchema);
 
@@ -47,7 +48,7 @@ app.get('/register',function(req,res){
 app.post('/register',function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(!err){
@@ -61,7 +62,7 @@ app.post('/register',function(req,res){
 
 app.post('/login', function(req,res){
     const username = req.body.username;
-    const password= req.body.password;
+    const password= md5(req.body.password);
     User.findOne({email:username}, function(err,foundUser){
         if(err){
             console.log(err);
